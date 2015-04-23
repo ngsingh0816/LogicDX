3923744ce573d06ef60d4ac3846e2aeedba7ced3
Neil Singh <Neil@Neils-MacBook-Pro.local>
2015-04-23 16:12:41 -0500

    Initial Commit

---
+++ LogicDX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LogicDX
+//
+//  Created by Neil Singh on 4/23/15.
+//  Copyright (c) 2015 Neil Singh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LogicDX/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LogicDX
+//
+//  Created by Neil Singh on 4/23/15.
+//  Copyright (c) 2015 Neil Singh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
